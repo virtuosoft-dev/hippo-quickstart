55cf6d8c4f322f67292e4c00479a5d133d8f5975
Stephen J Carnam <steve@steveorevo.com>
2023-08-17 12:30:47 -0700

    wip

--- quickstart.php
+++ quickstart.php
@@ -29,7 +29,7 @@
             $content = '<footer ' . $hcpp->delLeftMost( $content, '<footer ');
             $content = '<div class="container">
                 Here is our Quickstart guide.
-            </div>';
+            </div>' . $content;
             $args['content'] = $content;
             return $args;
         }
@@ -52,8 +52,7 @@
                 <a class="main-menu-item-link' . $active . '" href="/list/web/?quickstart=true" title="Easy-to-use guide">
                     <p class="main-menu-item-label">QUICKSTART<i class="fas fa-flag-checkered"></i></p>
                     <ul class="main-menu-stats">
-                        <li> easy-to-use </li>
-                        <li> guide </li>
+                        <li> easy-to-use guide</li>
                     </ul>
                 </a>
             </li>';
