4c461ac7350e9580d95145ad206e8d05d07c730b
Stephen J Carnam <steve@steveorevo.com>
2024-04-01 09:00:07 -0700

    wip

--- remote.js
+++ remote.js
@@ -1,13 +1,16 @@
 (function($) {
     $(function() {
         // Make headings collapsible
-        $('h3.tb-heading').html('<span class="triangle-down"></span><a href="#">' + $('h3.tb-heading').text() + '</a>').click(function() {
-            $(this).next().toggle();
-            $(this).find('span').toggleClass('triangle-down triangle-up');
-            return false;
-        });
+        // $('h3.tb-heading').html('<span class="triangle-down"></span><a href="#">' + $('h3.tb-heading').text() + '</a>').click(function() {
+        //     $(this).next().toggle();
+        //     $(this).find('span').toggleClass('triangle-down triangle-up');
+        //     return false;
+        // });
 
-        // Post a message to the parent window with wrapper height
-        window.parent.postMessage( { "height": $('#wrapper').height() }, '*' );
+        // Post a message to the parent window that we're ready with wrapper height
+        window.parent.postMessage( {
+            "type": "ready",
+            "height": $('#wrapper').height()
+        }, 'local.dev.pw:8083' );
     });  
 })(jQuery);
