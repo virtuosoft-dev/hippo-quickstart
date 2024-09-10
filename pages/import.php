cdeae08da60189e91912927b2cea6642e24531ad
Stephen J Carnam <steve@steveorevo.com>
2024-09-09 22:31:55 -0700

    wip

--- pages/import.php
+++ pages/import.php
@@ -99,7 +99,7 @@
                 var formData = new FormData();
                 formData.append('file', file);
                 $.ajax({
-                    url: '../../quickstart-upload/?job_id=<?php echo $job_id; ?>',
+                    url: '/quickstart-upload/?job_id=<?php echo $job_id; ?>',
                     type: 'POST',
                     data: formData,
                     processData: false,
